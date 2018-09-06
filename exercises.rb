8a3e88b5c39538e0bb6f3f5bee548a81ca342c5e
Lukeveille <luke@lukeleveille.com>
2018-09-06 15:59:36 -0400

    Completed exercise 3

--- exercises.rb
+++ exercises.rb
@@ -2,13 +2,13 @@
 fav_colours = ["blue", "red", "green", "purple"]
 ages = [30, 27, 30, 33]
 coin = ["tails", "tails", "heads", "tails", "heads"]
-artists = ["Run The Jewels", "Kendrick Lamar", "Brockhampton"]
+artists = ["Run The Jewels", "Kendrick Lamar", "Brockhampton", "Bootsy Collins"]
 colours = [:blue, :red, :green, :purple]
 
 # Hashes
 words = {common: "occurring, found, or done often; prevalent.", assistant: "a person who ranks below a senior person.", precarious: "not securely held or in position; dangerously likely to fall or collapse."}
-movies = {goodfellas: "1990", pulp_fiction: "1994", twelve_monkeys: "1995"}
-cities = {toronto: "2.8 million", new_york_city: "1.7 million", chicago: "2.7 million"}
+movies = {"Goodfellas" => 1990, "Pulp Fiction" => 1994, "Twelve Monkeys" => 1995}
+cities = {toronto: "2.8 million", new_york_city: "8.5 million", chicago: "2.7 million"}
 names = {luke: 30, tish: 27, betts: 30, phillip: 33}
 
 # Exercise 1
@@ -16,7 +16,9 @@ puts coin
 puts fav_colours[0]
 puts ages.sort
 ages << 0
-puts movies[:goodfellas]
+print movies[:goodfellas]
+
+puts "--------------------------------------------"
 
 # Exercise 2
 puts fav_colours[fav_colours.length-1]
@@ -24,3 +26,13 @@ cities[:boston] = "673 k"
 coin.reverse!
 puts cities[:toronto]
 artists.each { |artist| puts "I really enjoy listening to #{artist}." }
+
+puts "--------------------------------------------"
+
+# Exercise 3
+artists[0, 2].each { |artist| puts "#{artist}" }
+movies.each { |movie, year| puts "#{movie} came out in #{year}" }
+reverse_names = names.sort_by {|name, age| age}.reverse
+puts reverse_names
+movies["Beauty and the Beast"] = [1991, 2017]
+puts movies["Beauty and the Beast"]
