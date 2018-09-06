08b1494ed8549d104928a8fb6b87f14d8d38ef32
Lukeveille <luke@lukeleveille.com>
2018-09-06 15:11:44 -0400

    Completed exercise 1

---
+++ exercises.rb
@@ -0,0 +1,18 @@
+# Arrays
+fav_colours = ["blue", "red", "green", "purple"]
+ages = [30, 27, 30, 33]
+coin = ["tails", "tails", "heads", "tails", "tails"]
+artists = ["Run The Jewels", "Kendrick Lamar", "Brockhampton"]
+colours = [:blue, :red, :green, :purple]
+
+# Hashes
+words = {common: "occurring, found, or done often; prevalent.", assistant: "a person who ranks below a senior person.", precarious: "not securely held or in position; dangerously likely to fall or collapse."}
+movies = {goodfellas: "1990", pulp_fiction: "1994", twelve_monkeys: "1995"}
+cities = {toronto: "2.8 million", new_york_city: "1.7 million", chicago: "2.7 million"}
+names = {luke: 30, tish: 27, betts: 30, phillip: 33}
+
+puts coin
+puts fav_colours[0]
+puts ages.sort
+ages << 0
+puts movies[:goodfellas]
